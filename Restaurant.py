e9070f0079e81e66b3d8d4a1214f8ed016cdbb76
Serhii Usenko <Usenko.uss@gmail.com>
2018-03-29 12:29:22 +0300

    Update Restaurant.py

--- Restaurant.py
+++ Restaurant.py
@@ -1,6 +1,9 @@
 import random
 import numpy
 
+def random_cook_time():
+    return random.randint(0, 100)
+
 dishes_string = 'spam, eggs,  tomato juice,   ham, eggs, eggs, ham, ham, ham, ham'
 # dishes_string = input('Make order please \n')
 dish_list = []
@@ -9,5 +12,5 @@ for dish in dishes_string.title().split(','):
     dish_list.append(dish.strip())
 unique_dish_list = numpy.unique(dish_list)
 
-for unique_dish_list in unique_dish_list:
-    print(unique_dish_list.ljust(20, '.'), random.randint(0, 100), 'min')
+for unique_dish in unique_dish_list:
+    print(unique_dish.ljust(20, '.'), random_cook_time(), 'min')
